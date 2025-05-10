5e392872bd105a6e84655cdd49a65e8c2b0df27b
Ramarosandratana Nomenjanahary <nomenjanahary090@gmail.com>
2025-05-10 20:37:29 +0300

    Add files via upload

---
+++ Point.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisiondirection
+{
+    public class Point
+    {
+        private int x { get; set; }
+        private int y { get; set; }
+
+        public Point (int X, int Y)
+        {
+            this.x = X;
+            this.y = Y;
+        }
+
+        public void Nord()
+        {
+            this.y--;
+        }
+        public void Sud()
+        {
+            this.y++;
+        }
+        public void Est()
+        {
+            this.x++;
+        }
+        public void Ouest()
+        {
+            this.x--;
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine($" X = {this.x} et Y = {this.y}");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+namespace Revisiondirection
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("X: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Y: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("Direction: ");
+            string direction = Console.ReadLine().ToUpper();
+
+            Point p1 = new Point(x, y);
+            for (int i = 0; i < direction.Length; i++)
+            {
+                switch (direction[i])
+                {
+                    case 'N':
+                        p1.Nord();
+                        break;
+                    case 'S':
+                        p1.Sud();
+                        break;
+                    case 'E':
+                        p1.Est();
+                        break;
+                    case 'O':
+                        p1.Ouest();
+                        break;
+                    default:
+                        Console.WriteLine("Verifier bien votre Direction");
+                        break;
+                }
+            }
+            p1.Afficher();
+        }
+    }
+}
